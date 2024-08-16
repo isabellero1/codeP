35f1921f6ae563a7328cb45c4e1eb097f3c56c10
IsabelleRo <isabelle.ro@du.edu>
2024-08-16 19:06:08 -0400

    anagramDup & twoIntSum solved

---
+++ codePractice/problems/arrayAndHashing/containsDup.js
@@ -0,0 +1,15 @@
+/*
+Given an integer array nums, 
+return true if any value appears more than once in the array, 
+otherwise return false.
+*/
+
+
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    
+    hasDuplicate(nums) {}
+}
---
+++ codePractice/problems/arrayAndHashing/isAnagram.js
@@ -0,0 +1,20 @@
+/*
+Given two strings s and t, 
+return true if the two strings are anagrams of each other, 
+otherwise return false.
+
+An anagram is a string that contains the exact same characters 
+as another string, but the order of the characters can be different.
+
+>>Constraints:
+s and t consist of lowercase English letters.
+*/
+
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {}
+}
