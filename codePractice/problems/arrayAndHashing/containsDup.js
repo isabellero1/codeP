803702b2ae3d56ffc66745c252c16fd803711384
IsabelleRo <isabelle.ro@du.edu>
2024-08-16 19:51:28 -0400

    isAnagram and containsDup solved

--- codePractice/problems/arrayAndHashing/containsDup.js
+++ codePractice/problems/arrayAndHashing/containsDup.js
@@ -11,5 +11,20 @@ class Solution {
      * @return {boolean}
      */
     
-    hasDuplicate(nums) {}
+    hasDuplicate(nums) {
+        const dict = {};
+        for(let i = 0; i < nums.length; i++){
+            //if dict already has the value as a key
+            if(dict[nums[i]]){
+                //there is a duplicate, so return true
+                return true;
+            }else{
+                //add it to the dict if it does not exist
+                dict[nums[i]] = 0;
+            }
+        }
+
+        //if it finishes the for loop without return true, there are no duplicates
+        return false;
+    }
 }
--- codePractice/problems/arrayAndHashing/isAnagram.js
+++ codePractice/problems/arrayAndHashing/isAnagram.js
@@ -16,5 +16,50 @@ class Solution {
      * @param {string} t
      * @return {boolean}
      */
-    isAnagram(s, t) {}
+    isAnagram(s, t) {
+
+        //if they dont have the same length, false
+        if(s.length != t.length){
+            return false;
+        }
+
+        const sLetters = {};
+        const tLetters = {};
+
+        for(let i = 0; i < s.length; i++){
+            //store string s and t letters into object w/ freq
+
+            if(sLetters[s[i]]){
+                //increase freq if letter already exists
+                sLetters[s[i]] += 1;
+            }else{
+                //create if it doesn't exist in obj
+                sLetters[s[i]] = 0
+            }
+
+            if(sLetters[t[i]]){
+                //increase freq if letter already exists
+                sLetters[t[i]] += 1;
+            }else{
+                //create if it doesn't exist in obj
+                sLetters[t[i]] = 0
+            }
+        }
+
+        //compare
+
+        for(const key in sLetters){
+            if(sLetters[key] != tLetters[key]){
+                return false;
+            }
+        }
+
+        //if it passes for loop, true
+        return true;
+    }
 }
+
+const s = "jar"
+const t = "jam"
+const func = new Solution();
+console.log(func.isAnagram(s,t))
